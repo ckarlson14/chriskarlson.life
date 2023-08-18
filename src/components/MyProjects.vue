c2151f82f15aa26e038132d6deb89c943095a38a
Chris <ckarlson14@gmail.com>
2023-08-18 10:04:06 -0700

    secondtap

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -3,8 +3,7 @@
     <div class="titles">
       <h5>What I'm working on</h5>
     </div>
-    <div v-for="(item, index) in this.projects"
-          :key="index">
+    <div v-for="(item, index) in this.projects" :key="index">
       <b-link :to="item.url" target="_blank">
         <b-card no-body style="display: inline-flex" class="mt-3 projects">
           <b-card-body>
@@ -12,9 +11,7 @@
               <img :src="item.logoFile" class="logo">
               <span class="title">
                 {{ item.name }}
-                <span v-for="(social, i) in item.social"
-                     :key="i"
-                 class="m-1">
+                <span v-for="(social, i) in item.social" :key="i" class="m-1">
                   <a :href="social.url" target="_blank" :class="social.icon"></a>
                 </span>
               </span>
@@ -39,133 +36,133 @@
 export default {
   name: "MyProjects.vue",
   data() {
-    return{
-      projects:  [{
+    return {
+      projects: [{
         id: "1",
+        status: "current",
+        logoFile: "/assets/secondtap.png",
+        name: "SecondTap",
+        subtitle: "tap as close to the second as you can",
+        site: "Join",
+        url: "https://secondtap.app/join",
+        social: [],
+        description:
+          "iOS game",
+        skills: ["devicon-swift-plain-wordmark colored", "devicon-firebase-plain-wordmark colored", "devicon-nodejs-plain-wordmark colored", "devicon-express-original-wordmark colored"]
+      }, {
+        id: "2",
         status: "current",
         logoFile: "/assets/weekof.png",
         name: "WeekOf",
         subtitle: "see who's free to hang out",
         site: "Join",
         url: "https://weekof.app/join",
-        social: [{url: "https://twitter.com/weekofapp", icon: "fa fa-twitter"}],
+        social: [{ url: "https://twitter.com/weekofapp", icon: "fa fa-twitter" }],
         description:
-            "iOS app",
-        skills: ["devicon-react-original-wordmark colored","devicon-nodejs-plain-wordmark colored","devicon-express-original-wordmark colored", "devicon-redis-plain-wordmark colored","devicon-mongodb-plain-wordmark colored","devicon-heroku-plain-wordmark colored"]
-      },{
-        id: "2",
+          "iOS app",
+        skills: ["devicon-react-original-wordmark colored", "devicon-nodejs-plain-wordmark colored", "devicon-express-original-wordmark colored", "devicon-redis-plain-wordmark colored", "devicon-mongodb-plain-wordmark colored", "devicon-heroku-plain-wordmark colored"]
+      }, {
+        id: "3",
         status: "current",
         logoFile: "/assets/clipsaver.png",
         name: "ClipSaver",
         subtitle: "Save and discover haircuts",
         site: "Join",
         url: "https://www.clipsaver.app/join",
-        social: [{url: "https://www.instagram.com/clipsaver.app", icon: "fa fa-instagram"}],
+        social: [{ url: "https://www.instagram.com/clipsaver.app", icon: "fa fa-instagram" }],
         description:
-            "iOS/Android app",
-        skills: ["devicon-react-original-wordmark colored","devicon-nodejs-plain-wordmark colored","devicon-express-original-wordmark colored", "devicon-redis-plain-wordmark colored","devicon-mongodb-plain-wordmark colored","devicon-heroku-plain-wordmark colored", "devicon-amazonwebservices-plain-wordmark colored"]
-      },{
-        id: "3",
+          "iOS/Android app",
+        skills: ["devicon-react-original-wordmark colored", "devicon-nodejs-plain-wordmark colored", "devicon-express-original-wordmark colored", "devicon-redis-plain-wordmark colored", "devicon-mongodb-plain-wordmark colored", "devicon-heroku-plain-wordmark colored", "devicon-amazonwebservices-plain-wordmark colored"]
+      }, {
+        id: "4",
         status: "current",
         logoFile: "/assets/ghoulchat.png",
         name: "ghoulchat",
         subtitle: "Anonymous group chats",
         site: "App Store",
         url: "https://apps.apple.com/us/app/ghoulchat/id1607021397",
-        social: [{url: "https://twitter.com/ghoulchatapp", icon: "fa fa-twitter"}],
+        social: [{ url: "https://twitter.com/ghoulchatapp", icon: "fa fa-twitter" }],
         description:
-            "iOS app",
-        skills: ["devicon-swift-plain-wordmark colored","devicon-nodejs-plain-wordmark colored","devicon-express-original-wordmark colored", "devicon-socketio-original-wordmark colored", "devicon-redis-plain-wordmark colored","devicon-mongodb-plain-wordmark colored","devicon-heroku-plain-wordmark colored"]
+          "iOS app",
+        skills: ["devicon-swift-plain-wordmark colored", "devicon-nodejs-plain-wordmark colored", "devicon-express-original-wordmark colored", "devicon-socketio-original-wordmark colored", "devicon-redis-plain-wordmark colored", "devicon-mongodb-plain-wordmark colored", "devicon-heroku-plain-wordmark colored"]
       },
       {
-        id: "4",
+        id: "5",
         status: "current",
         logoFile: "/assets/wewake.png",
         name: "weWake",
         subtitle: "Alarm clock for friends",
         site: "App Store",
         url: "https://apps.apple.com/us/app/wewake/id1495745797",
-        social: [{url: "https://instagram.com/wewakeapp", icon: "fa fa-instagram"}, {url: "https://twitter.com/wewakeapp", icon: "fa fa-twitter"}],
+        social: [{ url: "https://instagram.com/wewakeapp", icon: "fa fa-instagram" }, { url: "https://twitter.com/wewakeapp", icon: "fa fa-twitter" }],
         description:
-            "iOS app",
-        skills: ["devicon-swift-plain-wordmark colored","devicon-python-plain-wordmark colored","devicon-flask-original-wordmark colored","devicon-sqlalchemy-original-wordmark colored","devicon-amazonwebservices-plain-wordmark colored","devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored"]
+          "iOS app",
+        skills: ["devicon-swift-plain-wordmark colored", "devicon-python-plain-wordmark colored", "devicon-flask-original-wordmark colored", "devicon-sqlalchemy-original-wordmark colored", "devicon-amazonwebservices-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored"]
       },
-        {
-          id: "5",
-          status: "current",
-          logoFile: "/assets/birthpay.png",
-          name: "birthpay",
-          subtitle: "Schedule gifts and birthday reminders",
-          site: "App Store",
-          url: "https://apps.apple.com/us/app/birthpay/id1575770355",
-          social: [{url: "https://instagram.com/birthpay_app", icon: "fa fa-instagram"}, {url: "https://twitter.com/birthpay_app", icon: "fa fa-twitter"}],
-          description:
-              "iOS app",
-          skills: ["devicon-swift-plain-wordmark colored","devicon-python-plain-wordmark colored","devicon-flask-original-wordmark colored","devicon-heroku-plain-wordmark colored", "devicon-sqlalchemy-original-wordmark colored", "devicon-postgresql-plain-wordmark colored","devicon-redis-plain-wordmark colored","devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored"]
-        },
-        {
-          id: "6",
-          status: "current",
-          logoFile: "/assets/iNFTsta.png",
-          name: "iNFTsta",
-          subtitle: "Turn your Instagram posts into NFTs, for free",
-          site: "",
-          url: "https://www.inftsta.com",
-          social: [{url: "https://twitter.com/inftsta", icon: "fa fa-twitter"}],
-          description:
-              "Web3 Ethereum Dapp",
-          skills: [ "devicon-python-plain-wordmark colored","devicon-flask-original-wordmark colored", "devicon-heroku-plain-wordmark colored", "devicon-facebook-plain colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-bootstrap-plain-wordmark colored", "devicon-javascript-plain colored", "devicon-jquery-plain-wordmark colored", "devicon-sqlalchemy-original-wordmark colored", "devicon-postgresql-plain-wordmark colored"]
-        },
-        {
-          id: "7",
-          status: "current",
-          logoFile: "/assets/potw.png",
-          name: "playoftheweek",
-          subtitle: "NCAA plays of the week",
-          site: "",
-          url: "https://playoftheweek.com",
-          social: [{url: "https://instagram.com/theplayoftheweek", icon: "fa fa-instagram"}],
-          description:
-              "Website",
-          skills: ["devicon-vuejs-plain-wordmark colored", "devicon-yarn-plain-wordmark colored\"", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-javascript-plain colored" ]
-        },
-        {
-          id: "8",
-          status: "current",
-          logoFile: "/assets/gerseyz.png",
-          name: "GERSEYZ",
-          subtitle: "Golf Jerseys",
-          site: "",
-          url: "https://gerseyz.com",
-          social: [{url: "https://instagram.com/gerseyz", icon: "fa fa-instagram"}],
-          description:
-              "Shopify Store",
-          skills: ["fa fa-instagram"]
-        },
-        {
-          id: "9",
-          status: "current",
-          logoFile: "/assets/openAll.png",
-          name: "OpenAll",
-          subtitle: "Open all your tabs from a folder in one click",
-          site: "",
-          url: "https://chrome.google.com/webstore/detail/openall/oggmebnpooffnoofbbcmgdogdpjcffpo?hl=en&authuser=1",
-          description:
-              "Chrome Extension",
-          skills: ["devicon-google-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-javascript-plain colored"]
-        },
-        {
-          id: "10",
-          status: "current",
-          logoFile: "/assets/hackerComments.png",
-          name: "HackerComments",
-          subtitle: "Open a hackerNews article's comment section after clicking through to the page",
-          site: "",
-          url: "https://chrome.google.com/webstore/detail/hacker-comments/didaegjbjcalcoiiiioecmkcfchhjanc?hl=en&authuser=1",
-          description:
-              "Chrome Extension",
-          skills: ["devicon-google-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-javascript-plain colored"]
-        },
+      {
+        id: "6",
+        status: "current",
+        logoFile: "/assets/birthpay.png",
+        name: "birthpay",
+        subtitle: "Schedule gifts and birthday reminders",
+        site: "App Store",
+        url: "https://apps.apple.com/us/app/birthpay/id1575770355",
+        social: [{ url: "https://instagram.com/birthpay_app", icon: "fa fa-instagram" }, { url: "https://twitter.com/birthpay_app", icon: "fa fa-twitter" }],
+        description:
+          "iOS app",
+        skills: ["devicon-swift-plain-wordmark colored", "devicon-python-plain-wordmark colored", "devicon-flask-original-wordmark colored", "devicon-heroku-plain-wordmark colored", "devicon-sqlalchemy-original-wordmark colored", "devicon-postgresql-plain-wordmark colored", "devicon-redis-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored"]
+      },
+      {
+        id: "7",
+        status: "current",
+        logoFile: "/assets/iNFTsta.png",
+        name: "iNFTsta",
+        subtitle: "Turn your Instagram posts into NFTs, for free",
+        site: "",
+        url: "https://www.inftsta.com",
+        social: [{ url: "https://twitter.com/inftsta", icon: "fa fa-twitter" }],
+        description:
+          "Web3 Ethereum Dapp",
+        skills: ["devicon-python-plain-wordmark colored", "devicon-flask-original-wordmark colored", "devicon-heroku-plain-wordmark colored", "devicon-facebook-plain colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-bootstrap-plain-wordmark colored", "devicon-javascript-plain colored", "devicon-jquery-plain-wordmark colored", "devicon-sqlalchemy-original-wordmark colored", "devicon-postgresql-plain-wordmark colored"]
+      },
+
+      {
+        id: "8",
+        status: "current",
+        logoFile: "/assets/gerseyz.png",
+        name: "GERSEYZ",
+        subtitle: "Golf Jerseys",
+        site: "",
+        url: "https://gerseyz.com",
+        social: [{ url: "https://instagram.com/gerseyz", icon: "fa fa-instagram" }],
+        description:
+          "Shopify Store",
+        skills: ["fa fa-instagram"]
+      },
+      {
+        id: "9",
+        status: "current",
+        logoFile: "/assets/openAll.png",
+        name: "OpenAll",
+        subtitle: "Open all your tabs from a folder in one click",
+        site: "",
+        url: "https://chrome.google.com/webstore/detail/openall/oggmebnpooffnoofbbcmgdogdpjcffpo?hl=en&authuser=1",
+        description:
+          "Chrome Extension",
+        skills: ["devicon-google-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-javascript-plain colored"]
+      },
+      {
+        id: "10",
+        status: "current",
+        logoFile: "/assets/hackerComments.png",
+        name: "HackerComments",
+        subtitle: "Open a hackerNews article's comment section after clicking through to the page",
+        site: "",
+        url: "https://chrome.google.com/webstore/detail/hacker-comments/didaegjbjcalcoiiiioecmkcfchhjanc?hl=en&authuser=1",
+        description:
+          "Chrome Extension",
+        skills: ["devicon-google-plain-wordmark colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-javascript-plain colored"]
+      },
       ]
     }
   },
@@ -173,16 +170,16 @@ export default {
 </script>
 
 <style scoped>
-
-.title{
+.title {
   font-size: 1.2rem;
   font-weight: 500;
 }
-.projects{
-  width:70%;
+
+.projects {
+  width: 70%;
 }
 
-.projects:hover{
+.projects:hover {
   background-color: cyan;
 }
 
@@ -196,7 +193,7 @@ img {
   display: inline-flex;
 }
 
-.body{
+.body {
   display: flex;
   flex-wrap: wrap;
   margin-left: 10px;
@@ -208,27 +205,28 @@ ul {
   margin-bottom: 0px;
 }
 
-.logo{
+.logo {
   max-height: 2rem;
   vertical-align: middle;
   border-radius: 25%;
 }
 
-.logo:hover{
+.logo:hover {
   opacity: 0.7;
 }
 
-.logoLink{
+.logoLink {
   display: flex;
   align-items: center;
 }
 
-a{
+a {
   text-decoration: none;
 }
 
-fa-twitter:hover, .fa-instagram:hover{
-  color:#e853e7;
+fa-twitter:hover,
+.fa-instagram:hover {
+  color: #e853e7;
 
 }
 
@@ -240,13 +238,11 @@ fa-twitter:hover, .fa-instagram:hover{
   -webkit-background-clip: text;
 }
 
-.fa-twitter{
+.fa-twitter {
   color: #55acee;
 }
 
-.fa-twitter:hover{
+.fa-twitter:hover {
   color: #55acee;
 }
-
-
 </style>
--- src/components/MySkills.vue
+++ src/components/MySkills.vue
@@ -16,15 +16,14 @@ export default {
   name: "MySkills.vue",
   data() {
     return {
-      skills: ["devicon-swift-plain-wordmark colored","devicon-nodejs-plain-wordmark colored", "devicon-vuejs-plain-wordmark colored","devicon-python-plain-wordmark colored", "devicon-express-original-wordmark colored","devicon-flask-original-wordmark colored","devicon-javascript-plain colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-bootstrap-plain-wordmark colored", "devicon-jquery-plain-wordmark colored", "devicon-npm-original-wordmark colored", "devicon-yarn-plain-wordmark colored","devicon-mongodb-plain-wordmark colored","devicon-sqlalchemy-original-wordmark colored","devicon-postgresql-plain-wordmark colored", "devicon-heroku-plain-wordmark colored", "devicon-amazonwebservices-plain-wordmark colored", "devicon-redis-plain-wordmark colored", "devicon-socketio-original-wordmark colored", "devicon-java-plain-wordmark colored", "devicon-matlab-plain colored", "devicon-arduino-plain-wordmark colored","devicon-vscode-plain-wordmark colored","devicon-xcode-plain colored", "devicon-github-original-wordmark colored","devicon-jira-plain-wordmark colored", "devicon-confluence-original-wordmark colored"]
+      skills: ["devicon-swift-plain-wordmark colored", "devicon-nodejs-plain-wordmark colored", "devicon-vuejs-plain-wordmark colored", "devicon-python-plain-wordmark colored", "devicon-express-original-wordmark colored", "devicon-flask-original-wordmark colored", "devicon-javascript-plain colored", "devicon-html5-plain-wordmark colored", "devicon-css3-plain-wordmark colored", "devicon-bootstrap-plain-wordmark colored", "devicon-jquery-plain-wordmark colored", "devicon-npm-original-wordmark colored", "devicon-yarn-plain-wordmark colored", "devicon-firebase-plain-wordmark colored", "devicon-mongodb-plain-wordmark colored", "devicon-sqlalchemy-original-wordmark colored", "devicon-postgresql-plain-wordmark colored", "devicon-heroku-plain-wordmark colored", "devicon-amazonwebservices-plain-wordmark colored", "devicon-redis-plain-wordmark colored", "devicon-socketio-original-wordmark colored", "devicon-java-plain-wordmark colored", "devicon-matlab-plain colored", "devicon-arduino-plain-wordmark colored", "devicon-vscode-plain-wordmark colored", "devicon-xcode-plain colored", "devicon-github-original-wordmark colored", "devicon-jira-plain-wordmark colored", "devicon-confluence-original-wordmark colored"]
     }
   }
 }
 </script>
 
 <style scoped>
-
-.icons{
+.icons {
   max-width: 70%;
   display: flex;
   flex-wrap: wrap;
@@ -38,5 +37,4 @@ export default {
   display: flex;
   flex-wrap: wrap;
 }
-
 </style>
